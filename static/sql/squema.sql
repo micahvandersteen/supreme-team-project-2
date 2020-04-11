7b4232ee87287c184a23e1a4f1384536594f3208
pdswanson <57888286+pdswanson@users.noreply.github.com>
2020-04-11 12:26:23 -0500

    Add files via upload
    Append columns

--- static/sql/squema.sql
+++ static/sql/squema.sql
@@ -10,7 +10,18 @@ CREATE TABLE DISTRICTS (
     generalFundExpends NUMERIC,
     perPupilExpends NUMERIC,
     avgTeacherSalary INT,
-    readingProficiency NUMERIC
+    readingProficiency NUMERIC,
+    Total_Enroll NUMERIC,
+    Female_Pct NUMERIC,
+    Male_Pct NUMERIC,
+    AIAN_Pct NUMERIC,
+    Asian_Pct NUMERIC,
+    NHPI_Pct NUMERIC,
+    HisLa_Pct NUMERIC,
+    Black_Pct NUMERIC,
+    White_Pct NUMERIC,
+    TwoMo_Pct NUMERIC,
+    Meals_Pct NUMERIC
 );
 
 /* from districts.geojson */
