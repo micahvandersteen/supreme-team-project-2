6e9d3ee9674e21615049b4bd01d75a9fef0b218b
boar1ang <55640944+boar1ang@users.noreply.github.com>
2020-04-17 15:22:40 -0500

    Update app.py

--- static/app.py
+++ static/app.py
@@ -1,8 +1,3 @@
-# DATABASE_URL will contain the database connection string:
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
-# Connects to the database using the app config
-db = SQLAlchemy(app)
-=======
 import os
 
 from sqlalchemy import create_engine, MetaData, select
