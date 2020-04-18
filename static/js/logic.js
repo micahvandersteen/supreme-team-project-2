640390a0d7ad8feeb5261e75c9ce0e49ca36d358
Micah Vandersteen <micahvandersteen@gmail.com>
2020-04-17 20:19:13 -0500

    adding final updates from micah's branch

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,212 +1,117 @@
-//js file for DEFAULT MAP DISPLAY
 
+// ======================
+// SETTING UP BASE MAP
+// ======================
 //Initialize map object:
 let myMap = L.map("map", {
-    center: [46.3527, -94.2020],
-    zoom: 6.5
-    });
+  center: [46.3527, -94.2020],
+  zoom: 6.5
+  });
 
 //Create & load tile layer:
 var lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 10,
-    minZoom: 4,
-    id: "mapbox.light",
-    accessToken: accessToken
-    }).addTo(myMap);
-
-// Adding tile layer
-var streets = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 11,
-  id: "mapbox.streets",
+  maxZoom: 10,
+  minZoom: 4,
+  id: "mapbox.light",
   accessToken: accessToken
   }).addTo(myMap);
 
-  //Create baseMaps object:
-  var baseMaps = {
-      "Light Map" : lightmap,
-      "Street Map" : streets
-      };
-
-  // maybe we should put this in the CSS
-  // var mapStyle = {
-  //   color: "#00661a",
-  //   fillColor: "#d7bde2",
-  //   fillOpacity: 0.6,
-  //   weight: 1.0
-  //     };
-
-// const dataFile = "static/data/bdry_school_district_boundaries.gpkg"
-// var districts = L.geoPackageFeatureLayer([], {
-//   geoPackageUrl: "static/data/bdry_school_district_boundaries.gpkg",
-//   layerName: 'Districts',
-//   style: {color: 'blue'}
-// }).addTo(myMap)
-
-// District Boundaries Layer 
-var districts = L.esri.featureLayer({
-  url: 'https://services.arcgis.com/GXwOsvnLQI6EDOp7/ArcGIS/rest/services/Minnesota_School_District_Boundaries_2020/FeatureServer/0',
-  simplifyFactor: 0.5,
-  precision: 5,
-  style: {
-      color: 'blue',
-      weight: 1
-  }
+// Adding tile layer
+var streets = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+maxZoom: 11,
+id: "mapbox.streets",
+accessToken: accessToken
 }).addTo(myMap);
 
-var overlayMaps = {
-  "Districts": districts
+var baseMaps = {
+  "Light" : lightmap,
+  "Streets" : streets
 };
 
-L.control.layers(baseMaps, overlayMaps, {
-  collapsed: false
-}).addTo(myMap);
+// ======================
+// END BASE MAP SETUP
+// ======================
+
+ // =========================================
+
+// Defining file path of geojson data file used to draw district boundaries
+const dataFile = "static/data/newdistrictboundaries.geojson";
+
+// ==================================================
+// Drawing district boundaries on map ** PROMISE **
+// ==================================================
+
+    // =======================================
+    // adding special info tab in upper corner
+    // ========================================
+     // custom info control to show popup info on hover
+     var info = L.control();
+
+     info.onAdd = function (myMap) {
+         this._div = L.DomUtil.create('div', 'info'); // div with class info
+         this.update();
+         return this._div;
+     };
+ 
+     // method to update info control based on the district hovered on
+     // change so that it shows variable that is selected in layer
+     info.update = function (props) {
+         this._div.innerHTML = '<h4> School District: </h4>' + (props ? '<b>' + props.NAME + '</b> <br />' 
+                                                                + props.totalEnrollment + ' Students Enrolled <br/>' 
+                                                                + '$' + props.medianHouseholdIncome + ' Median Household Income <br/>' 
+                                                                + '$' + props.teacherSalaries + ' Mean Teacher Salary <br/>' 
+                                                                + '$' + props.perPupilExpenditure + ' per Pupil Expenditure <br/>'
+                                                                + props.readingProficiencyScores.toFixed(2) + '% Reading Proficiency'
+         : 'Hover over a district');
+     }
+ 
+     //add to map
+     info.addTo(myMap); 
 
 
-  // d3.json(dataFile).then((data) => {
-  //   function onEachFeature(feature, layer) {
-  //     layer.bindPopup("<h6>" + feature.properties.UNI_NAM + "</h6><hr><h6>Avg. Enrollment:&nbsp;" + feature.properties.admtotal + "</h6>");
-  //     console.log(data);
-  //       }
-    
-  //   var geoFile = L.geoJson(data, {onEachFeature: onEachFeature, style: mapStyle});
-  //     geoFile.addTo(myMap);
+    // =======================================================
+    // ============== Legend stuff starts here ===============
+    // =======================================================
 
-      // chloropleth interactivity code starts here - Micah
-      
-      // getColor function start
-      // we are going to need to get the data we want to choose colors before this and change it based on the layer selected
-      // right now, they all show up beige
-      // ===== Function to get the color of the state based on thresholds we set and gradient we choose ======
-    //   function getColor(variableChosen) {
-    //     return variableChosen > "300" ? "darkred" :
-    //         variableChosen > "200" ? "crimson" :
-    //         variableChosen > "100" ? "red" :
-    //         variableChosen > "50" ? "orange" :
-    //         variableChosen > "10" ? "yellow" :
-    //                                 "beige";
-    //       }
-    //   // getColor function ends
-    
-    //   // ====================
-    //   // style function start
-    //   // ====================
-
-    //   // ===== Function to style each district by the variable chosen =====
-    //   function style(feature) {
-    //     return {
-    //         fillColor: getColor(feature.properties.density), // change what we pass through to this function to rep. selected layer variable
-    //         weight: 2,
-    //         opacity: 1,
-    //         color: 'white',
-    //         dashArray: '3',
-    //         fillOpacity: 0.7
-    //     };
-    //   }
-    //   // ===== adding style to map =====
-    //   L.geoJson(data, {style: style}).addTo(myMap); // change data
-
-    //   // ==================================
-    //   // style function & adding style ends
-    //   // ==================================
+    // ==================================
+    // Defining grades object for legend
+    // ==================================
+    var gradesTotalEnrollment = [0,7790,15543,23296,31049,38802],
+      gradesperPupilExpenditure = [6598,9786,12973,16161,19348,22536],
+      gradesreadingProficiencyScores = [22.4,35.6,48.8,62,75.2,88.5],
+      gradesteacherSalaries = [31994,40693,49392,58091,66790,75489],
+      gradesmedianHouseholdIncome = [26313,44909,63505,82102,100697,119294];
 
 
-    //   // =================================
-    //   // highlighting features starts here
-    //   // =================================
-    //   // ===== adding interaction to make districts highlight when hovered on =====
-    //   // need to go over with instructor, not working as expected but also not giving errors
-    //   function highlightFeature(e) {
+    var legend = L.control({position: 'bottomright'});
 
-    //     // sets targert layer
-    //     var layer = e.target;
+    legend.onAdd = function (myMap) {
 
-    //     // creates style for target layer
-    //     layer.setStyle({
-    //         weight: 5,
-    //         color: '#666',
-    //         dashArray: '',
-    //         fillOpacity: 0.7
-    //     });
+      var div = L.DomUtil.create('div', 'info legend'),
+          // change so grades change by selected variable
+          grades = gradesTotalEnrollment, // CHANGE THIS TO SELECT GRADE FROM GRADE OBJECTS
+          labels = [];
 
-    //     // brings layer to front on mouseover
-    //     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-    //         layer.bringToFront();
-    //     }
+      // loop through intervals 'grades' and generate label
+      for (var i = 0; i < grades.length - 1; i++) {
+        div.innerHTML += 
+        '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      } 
 
-    //     info.update(layer.feature.properties);
-    //   }
+      return div;
 
-    //   // resets styling of previously highlighted layer on mouseout
-    //   function resetHighlight(e) {
-    //     geojson.resetStyle(e.target);
-    //     info.update();
-    //   }
+    };
 
-    //   // check this syntax
-    //   geojson = L.geoJson();
-
-    //   // ==============================
-    //   // hightlighting interaction ends
-    //   // ==============================
-
-    //   // ==============================
-    //   // zoom on click function start 
-    //   // ==============================
-    //   // function to zoom to selected district on click
-    //   function zoomToFeature(e) {
-    //     myMap.fitBounds(e.target.getBounds());
-    //   }
-    //   // ==============================
-    //   // zoom on click function end
-    //   // ==============================
+    legend.addTo(myMap);
 
 
-    //   // function to add event listeners to our event listener functions
-    //   // function to add listeners to district layer functions
-    //   function onEachFeature(feature, layer) {
-    //     layer.on({
-    //         mouseover: highlightFeature,
-    //         mouseout: resetHighlight,
-    //         click: zoomToFeature
-    //     });
-    //   }
-    //   // ===========================
-    //   // event listener function ends
-    //   // ============================
+// Adding Layer Control for Chorpleth layers
+// var overlayMaps = {
+//   "Total Enrollment" : geojson,
+//   };
 
-    //   // adding above to map
-    //   geojson = L.geoJson(data, {
-    //     style: style,
-    //     onEachFeature: onEachFeature
-    //   }).addTo(myMap);
+// L.control.layers(overlayMaps).addTo(myMap);
 
-    //   // =======================================
-    //   // adding special info tab in upper corner
-    //   // ========================================
-    //   // custom info control to show popup info on hover
-    //   var info = L.control();
-
-    //   info.onAdd = function (myMap) {
-    //       this._div = L.DomUtil.create('div', 'info'); // div with class info
-    //       this.update();
-    //       return this._div;
-    //   };
-
-    //   // method to update info control based on the district hovered on
-    //   info.update = function (props) {
-    //       this._div.innerHTML = '<h4> School District: </h4>' + (props ? '<b>' + props.UNI_NAM + '</b>' : 'Hover over a state');
-    //   }
-
-    //   //add to map
-    //   info.addTo(myMap);
-      
-
-
-
-    // });
-    
-      
-    
-  
